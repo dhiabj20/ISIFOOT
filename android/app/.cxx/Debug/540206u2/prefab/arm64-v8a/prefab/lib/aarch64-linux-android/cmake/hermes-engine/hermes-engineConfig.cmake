24742d51cc73ca3e73e359bebef4eadaadf4de16
Dhia <dhiabj99@gmail.com>
2026-05-02 14:55:33 +0100

    Cleanup: remove generated build artifacts from repository

--- android/app/.cxx/Debug/540206u2/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Mega-PC/.gradle/caches/8.10.2/transforms/d25fc2b2e845e45e1c22e9d8eda7b2cc/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Mega-PC/.gradle/caches/8.10.2/transforms/d25fc2b2e845e45e1c22e9d8eda7b2cc/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/q3o11116/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Mega-PC/.gradle/caches/8.11.1/transforms/8d4ff158e8c3d9f7b9cc70fe4be34fa5/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Mega-PC/.gradle/caches/8.11.1/transforms/8d4ff158e8c3d9f7b9cc70fe4be34fa5/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
